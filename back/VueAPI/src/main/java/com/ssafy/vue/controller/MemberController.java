c8f5c16bcdf669e7c1e81a00684e39aa06102315
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-05-25 10:41:32 +0900

    member cors

--- back/VueAPI/src/main/java/com/ssafy/vue/controller/MemberController.java
+++ back/VueAPI/src/main/java/com/ssafy/vue/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+@CrossOrigin("*")
 @Api("MemberController V1")
 @RestController
 @RequestMapping("/user")

56db5258d3659483fbccefc8a2695c81dac49b6a
swoody1101 <swoody1101@gmail.com>
2022-05-26 19:49:05 +0900

    map

--- front/src/components/house/HouseMap.vue
+++ front/src/components/house/HouseMap.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div id="map"></div>
+    <button @click="addMarker">불러와</button>
   </div>
 </template>
 
@@ -10,26 +11,45 @@ const houseStore = "houseStore";
 
 export default {
   data() {
-    return {};
+    return {
+      map: null,
+      markers: [],
+      geocoder: null,
+    };
   },
   computed: {
-    ...mapState(houseStore, ["houses", "sido", "gugun", "dong"]),
+    ...mapState(houseStore, ["houses", "house", "sido", "gugun", "dong"]),
+  },
+  created() {
+    console.log("maap created");
+    setTimeout(() => {
+      this.initMap();
+    }, 500);
+  },
+  watch: {
+    houses() {
+      this.addMarker();
+    },
+    house() {
+      this.detailMarker();
+    },
   },
   mounted() {
+    /* global kakao */
+    console.log("map mounted");
     if (window.kakao && window.kakao.maps) {
+      console.log("exists");
       this.initMap();
     } else {
       const script = document.createElement("script");
-      /* global kakao */
-      script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a4fdf6bad8dd4101f7624f1f9446e8ab&libraries=service";
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a4fdf6bad8dd4101f7624f1f9446e8ab&libraries=services";
       document.head.appendChild(script);
-      this.addMarker();
+      script.onload = () => {
+        kakao.maps.load(this.initMap);
+        // console.log("loaded");
+      };
     }
-  },
-  updated() {
-    console.log("updated 호출");
   },
   methods: {
     initMap() {
@@ -37,47 +57,41 @@ export default {
       var container = document.getElementById("map");
       var options = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
+        level: 5,
       };
 
-      var map = new kakao.maps.Map(container, options);
-      map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+      this.map = new kakao.maps.Map(container, options);
 
+      this.geocoder = new kakao.maps.services.Geocoder();
       var marker = new kakao.maps.Marker({
-        position: map.getCenter(),
+        position: this.map.getCenter(),
       });
-      marker.setMap(map);
-      this.addMarker();
+      marker.setMap(this.map);
     },
     addMarker() {
       console.log("addMarker 호출");
 
-      var container = document.getElementById("map");
-      var mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 4,
-      };
-      var map = new kakao.maps.Map(container, mapOption);
-      var geocoder = new kakao.maps.services.Geocoder();
-
+      // var geocoder = new kakao.maps.services.Geocoder();
+      let _this = this;
+      console.log("geocoder", _this.geocoder);
       if (this.houses.length == 0) {
         var address = this.sido + " " + this.gugun + " " + this.dong;
         console.log("목록이 없을 때 ", address);
 
-        geocoder.addressSearch(address, function (result, status) {
+        _this.geocoder.addressSearch(address, function (result, status) {
           if (status === kakao.maps.services.Status.OK) {
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
             var marker = new kakao.maps.Marker({
-              map: map,
+              map: _this.map,
               position: coords,
             });
             var infowindow = new kakao.maps.InfoWindow({
               content:
                 '<div style="width:150px;text-align:center;padding:6px 0;">해당 지역에 거래 내역이 없습니다.</div>',
             });
-            infowindow.open(map, marker); // 지도의 중심을 이동
-            marker.setMap(map);
-            map.setCenter(coords);
+            infowindow.open(_this.map, marker); // 지도의 중심을 이동
+            marker.setMap(_this.map);
+            _this.map.setCenter(coords);
           }
         });
       }
@@ -86,28 +100,54 @@ export default {
         console.log(house.address);
         console.log(houseAddress);
 
-        geocoder.addressSearch(houseAddress, function (result, status) {
+        _this.geocoder.addressSearch(houseAddress, function (result, status) {
           if (status === kakao.maps.services.Status.OK) {
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
             var marker = new kakao.maps.Marker({
-              map: map,
+              map: _this.map,
               position: coords,
               title: house.aptName,
             }); // 인포윈도우로 장소에 대한 설명을 표시합니다
             var infowindow = new kakao.maps.InfoWindow({
               content:
                 '<div style="width:150px;text-align:center;padding:6px 0;">' +
-                houseAddress +
-                " " +
                 house.aptName +
                 "</div>",
             });
-            infowindow.open(map, marker); // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-            marker.setMap(map);
-            map.setCenter(coords);
+            infowindow.open(_this.map, marker); // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            marker.setMap(_this.map);
+            _this.map.setCenter(coords);
           }
         });
       });
+    },
+    detailMarker() {
+      console.log("addMarker 호출");
+      let _this = this;
+      _this.geocoder.addressSearch(
+        _this.house.address,
+        function (result, status) {
+          if (status === kakao.maps.services.Status.OK) {
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            var marker = new kakao.maps.Marker({
+              map: _this.map,
+              position: coords,
+              title: _this.house.aptName,
+            });
+            var infowindow = new kakao.maps.InfoWindow({
+              content:
+                '<div style="width:150px;text-align:center;padding:6px 0; bold">' +
+                _this.house.address +
+                " " +
+                _this.house.aptName +
+                "</div>",
+            });
+            infowindow.open(_this.map, marker); // 지도의 중심을 이동
+            marker.setMap(_this.map);
+            _this.map.setCenter(coords);
+          }
+        },
+      );
     },
   },
 };
--- front/src/components/house/HouseSearchBar.vue
+++ front/src/components/house/HouseSearchBar.vue
@@ -21,7 +21,6 @@
         v-model="keyword"
         placeholder="아파트 이름을 입력하세요."
       />
-
       <button type="submit">검색</button>
     </form>
   </div>

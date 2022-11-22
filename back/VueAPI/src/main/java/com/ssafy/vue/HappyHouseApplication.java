8385472b09b6b24aa173a139f81687113d094bbe
swoody1101@gmail.com <swoody1101@gmail.com>
2022-11-22 16:28:48 +0900

    refactor: 실행 클래스 이름 변경

--- back/VueAPI/src/main/java/com/ssafy/vue/HappyHouseApplication.java
+++ back/VueAPI/src/main/java/com/ssafy/vue/HappyHouseApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VueApiApplication {
+public class HappyHouseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VueApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HappyHouseApplication.class, args);
+    }
 }
